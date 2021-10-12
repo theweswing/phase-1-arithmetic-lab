a7010a4973ea4a0d74292886c9716041fd9c3038
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-10-12 17:32:30 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1=31
+let num2=2
+let multiply = (num1*num2)
+
+
+
+
+let random = (Math.ceil(Math.random()))*10
+
+
+
+
+
+let num3=14
+let num4=5
+let mod= (num3 - (num4*(Math.floor(num3 / num4))))
+
+
+let max= Math.max(5,10,20)
